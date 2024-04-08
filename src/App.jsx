0ebcd4fdbf45b6c182a0547080abc2c1929d978a
GeorgeKuncheria <chempumthara.g@northeastern.edu>
2024-04-08 02:47:30 -0400

    complete project

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,8 @@ import Highlights from './components/highlights/index.jsx';
 import Pricing from './components/pricing/index.jsx';
 import Location from './components/location/index.jsx';
 
+import {Element} from 'react-scroll';
+
 
 
 const App = () => {
@@ -18,25 +20,25 @@ const App = () => {
           <Header/>
         </>
 
-        <>
+        <Element name="featured">
           <Featured />
-        </>
+        </Element>
 
-        <>
+        <Element name="venueInfo">
           <VenueInfo/>
-        </>
+        </Element>
 
-        <>
+        <Element name="highlights">
           <Highlights/>
-        </>
+        </Element>
 
-        <>
+        <Element name="pricing">
           <Pricing/>
-        </>
+        </Element>
 
-        <>
+        <Element name="location">
           <Location/>
-        </>
+        </Element>
         
         <>
           <Footer/>
--- src/components/header_footer/SideDrawer.jsx
+++ src/components/header_footer/SideDrawer.jsx
@@ -1,37 +1,49 @@
-import React from 'react'
+
+import React from 'react';
+import { scroller } from 'react-scroll'
 
 import { Drawer, List, ListItem } from '@mui/material'
 
 const SideDrawer = (props) => {
-  const links = [
-    { where:'featured',value:'To top'},
-    { where:'venuenfo',value:'Venue NFO'},
-    { where:'highlights',value:'Highlights'},
-    { where:'pricing',value:'Pricing'},
-    { where:'location',value:'Location'}
-];
-
-const renderItem = (item) => {
-  return (
-    <ListItem button onClick={()=> alert(item.where)} key={item.where}>
-        {item.value}
-    </ListItem>
-  )
-};
-
-  return (
-    <Drawer
-      anchor={"right"}
-      open={props.open}
-      onClose={()=> props.onClose(false)}
-    >
-    <List component="nav">
-        {links.map((item)=> renderItem(item) )}
-    </List>
+    const links = [
+        { where:'featured',value:'To top'},
+        { where:'venueInfo',value:'Venue INFO'},
+        { where:'highlights',value:'Highlights'},
+        { where:'pricing',value:'Pricing'},
+        { where:'location',value:'Location'}
+    ]
 
 
-    </Drawer>
-  )
+    const scrollToElement = (element) => {
+        scroller.scrollTo(element,{
+            duration:1500,
+            delay:100,
+            smooth: true,
+            offset:-80
+        });
+        props.onClose(false)
+    }
+
+    const renderItem = (item) => (
+        <ListItem button onClick={()=> scrollToElement(item.where)} key={item.where}>
+            {item.value}
+        </ListItem>
+    )
+
+
+    return(
+        <Drawer
+            anchor={"right"}
+            open={props.open}
+            onClose={()=> props.onClose(false)}
+        >
+            <List component="nav">
+                { links.map((item)=> renderItem(item) )}
+                
+            </List>
+        </Drawer>
+    )
+
 }
 
 export default SideDrawer;
